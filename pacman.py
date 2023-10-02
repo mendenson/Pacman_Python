0a4e7a9e3337dfc42418ccc16cbe299d04be401e
Mendenson Mota <mendenson@gmail.com>
2023-10-01 22:09:42 -0600

    Update pacman.py
    These are the code implementations:
    - Directional commands using arrow keys
    - Check for player collisions with walls and allowable turns

--- pacman.py
+++ pacman.py
@@ -21,6 +21,7 @@ player_x = 450
 player_y = 663
 direction = 0
 counter = 0
+flicker = False
 
 def draw_board():
     num1 = ((HEIGHT - 50) // 32)
@@ -29,7 +30,7 @@ def draw_board():
         for j in range(len(level[i])):
             if level[i][j] == 1:
                 pygame.draw.circle(screen, 'white', (j * num2 + (0.5 * num2), i * num1 + (0.5 * num1)), 4)
-            if level[i][j] == 2:
+            if level[i][j] == 2 and not flicker:
                 pygame.draw.circle(screen, 'white', (j * num2 + (0.5 * num2), i * num1 + (0.5 * num1)), 10)
             if level[i][j] == 3:
                 pygame.draw.line(screen, color, (j * num2 + (0.5 * num2), i * num1),
@@ -65,8 +66,11 @@ while run:
     timer.tick(fps)
     if counter < 19:
         counter += 1
+        if counter > 3:
+            flicker = False
     else:
         counter = 0
+        flicker = True
         
     screen.fill('black')
     draw_board()
@@ -75,6 +79,15 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                direction = 0
+            if event.key == pygame.K_LEFT:
+                direction = 1
+            if event.key == pygame.K_UP:
+                direction = 2
+            if event.key == pygame.K_DOWN:
+                direction = 3
     
     pygame.display.flip()
     
